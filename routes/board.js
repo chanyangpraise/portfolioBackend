db5507226907146cdeb533bc0b338c34535876a9
jg06 <jg06241@gmail.com>
2023-02-24 23:19:26 +0900

    board main 피드 조회 기능 추가

--- routes/board.js
+++ routes/board.js
@@ -42,47 +42,52 @@ router.post("/write", upload.single("image"), async (req, res) => {
 // select 사용자 닉네임 + 게시판 글 + 생성일자
 // http://localhost:3000/boadrd/get?id=1
 // http://localhost:3000/boadrd/get/1
-router.get("/get/:uid", async (req, res) => {
+router.get("/get/:uid", (req, res) => {
   const { uid } = req.params;
   let { page, count } = req.query;
   if (!uid) {
     res.status(400).end();
   }
   if (!count) {
-    count = 6;
+    count = 10;
   }
   if (!page || page < 1) {
     page = 0;
   }
 
-  try {
-    await asyncSQL(
-      `SELECT 
-        b.b_id as bid,
-        b.b_comment as content,
-        b.b_timg as thumbnail,
-        b.b_date as date
-      FROM Board b JOIN user a
-      ON b.b_uid = a.u_id
-      WHERE b_uid = "${uid}"
-      ORDER BY b_date DESC
-      LIMIT ${page * count}, ${count}`
-    );
-    res.status(200).json({
-      status: "success",
-      content: rows,
-      nextPage: Number(page) + 1, // 다음 페이지 번호
+  asyncSQL(
+    `SELECT 
+      b.b_id as bid,
+      b.b_comment as content,
+      b.b_timg as thumbnail,
+      b.b_date as date
+    FROM Board b JOIN user a
+    ON b.b_uid = a.u_id
+    WHERE b_uid = "${uid}"
+    ORDER BY b_date DESC
+    LIMIT ${page * count}, ${count}`,
+    // LIMIT a   -> a 개수 만큼만 들고 오겠다.
+    // LIMIT a , b -> a부터 b개만큼 들고 오겠다.
+    // page 0 count 10 -> 0, 10
+    // page 1 count 10 -> 10, 10
+  )
+    .then((rows) => {
+      res.status(200).json({
+        status: "success",
+        content: rows,
+        nextPage: Number(page) + 1, // 다음 페이지 번호
       nextCount: Number(count), // 다음 페이지에 보여줄 개수
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: "fail",
+        message: "서버에서 에러가 발생 하였습니다.",
+      });
+      if (process.env.NODE_ENV === "development") {
+        console.error(err);
+      }
     });
-  } catch (err) {
-    res.status(500).json({
-      status: "fail",
-      message: "서버에서 에러가 발생 했습니다.",
-    });
-    if (process.env.NODE_ENV === "development") {
-      console.error(err);
-    }
-  }
 });
 
 
@@ -259,8 +264,7 @@ router.get("/get/main", async (req, res) => {
     page = 0;
   }
 
-  try {
-    const rows = await asyncSQL(
+ await asyncSQL(
       `SELECT 
         b.b_id as bid,
         b.b_comment as content,
@@ -273,22 +277,24 @@ router.get("/get/main", async (req, res) => {
       ON b.b_uid = a.u_id
       ORDER BY b_date DESC
       LIMIT ${page * count}, ${count}`
-    );
-    res.status(200).json({
-      status: "success",
-      content: rows,
-      nextPage: Number(page) + 1, // 다음 페이지 번호
+  )
+    .then((rows) => {
+      res.status(200).json({
+        status: "success",
+        content: rows,
+        nextPage: Number(page) + 1, // 다음 페이지 번호
       nextCount: Number(count), // 다음 페이지에 보여줄 개수
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: "fail",
+        message: "서버에서 에러가 발생 하였습니다.",
+      });
+      if (process.env.NODE_ENV === "development") {
+        console.error(err);
+      }
     });
-  } catch (err) {
-    res.status(500).json({
-      status: "fail",
-      message: "서버에서 에러가 발생 했습니다.",
-    });
-    if (process.env.NODE_ENV === "development") {
-      console.error(err);
-    }
-  }
 });
 
 
--- test.js
+++ test.js
@@ -1,43 +1,49 @@
-router.get("/get/:uid", async (req, res) => {
-    const { uid } = req.params;
-    let { page, count } = req.query;
-    if (!uid) {
-      res.status(400).end();
-    }
-    if (!count) {
-      count = 6;
-    }
-    if (!page || page < 1) {
-      page = 0;
-    }
-  
-    try {
-      const rows = await asyncSQL(
-        `SELECT 
-          b.b_id as bid,
-          b.b_comment as content,
-          b.b_timg as thumbnail,
-          b.b_date as date
-        FROM Board b JOIN user a
-        ON b.b_uid = a.u_id
-        WHERE b_uid = "${uid}"
-        ORDER BY b_date DESC
-        LIMIT ${page * count}, ${count}`
-      );
+router.get("/get/:uid", (req, res) => {
+  const { uid } = req.params;
+  let { page, count } = req.query;
+  if (!uid) {
+    res.status(400).end();
+  }
+  if (!count) {
+    count = 10;
+  }
+  if (!page || page < 1) {
+    page = 0;
+  }
+
+  asyncSQL(
+    `SELECT 
+      b.b_id as bid,
+      b.b_comment as content,
+      b.b_timg as thumbnail,
+      b.b_date as date
+    FROM Board b JOIN user a
+    ON b.b_uid = a.u_id
+    WHERE b_uid = "${uid}"
+    ORDER BY b_date DESC
+    LIMIT ${page * count}, ${count}`,
+    // LIMIT a   -> a 개수 만큼만 들고 오겠다.
+    // LIMIT a , b -> a부터 b개만큼 들고 오겠다.
+    // page 0 count 10 -> 0, 10
+    // page 1 count 10 -> 10, 10
+  )
+    .then((rows) => {
       res.status(200).json({
         status: "success",
         content: rows,
         nextPage: Number(page) + 1, // 다음 페이지 번호
-        nextCount: Number(count), // 다음 페이지에 보여줄 개수
+      nextCount: Number(count), // 다음 페이지에 보여줄 개수
       });
-    } catch (err) {
+    })
+    .catch((err) => {
       res.status(500).json({
         status: "fail",
-        message: "서버에서 에러가 발생 했습니다.",
+        message: "서버에서 에러가 발생 하였습니다.",
       });
       if (process.env.NODE_ENV === "development") {
         console.error(err);
       }
-    }
-  });
+    });
+});
+
   
