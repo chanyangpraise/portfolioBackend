8117c9160a3dd9ab7882dd806e220a461a975b9c
jg06 <jg06241@gmail.com>
2023-02-28 17:21:55 +0900

    mail.js 수정

--- functions/mail.js
+++ functions/mail.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-function sendMail(email, digit, callback) {
+async function sendMail(email, digit) {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
@@ -18,32 +18,8 @@ function sendMail(email, digit, callback) {
     text: `당신의 인증 번호는 ${digit}입니다.`,
   };
 
-  transporter.sendMail(mailOptions, (err, info) => {
-    callback(err, info);
-  });
+  await transporter.sendMail(mailOptions);
 }
-// async function sendMail(email, digit) {
-//   console.log(email);
-//   console.log(digit);
-//   const transporter = nodemailer.createTransport({
-//     host: "smtp.gmail.com",
-//     post: 465,
-//     secure: true,
-//     auth: {
-//       user: process.env.MAIL_ID,
-//       pass: process.env.MAIL_PASSWORD,
-//     },
-//   });
-
-//   const mailOptions = {
-//     from: process.env.MAIL_ID,
-//     to: email,
-//     subject: "이메일 인증을 해 주세요.",
-//     text: `당신의 인증 번호는 ${digit}입니다.`,
-//   };
-
-//   await transporter.sendMail(mailOptions);
-// }
 
 function randomNumber() {
   return Math.floor(Math.random() * (999999 - 100000) + 100000);
