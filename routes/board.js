f3a8e8aec54414515ad178b056de07449eb96660
jg06 <jg06241@gmail.com>
2023-03-06 17:52:13 +0900

    0306-2

--- routes/board.js
+++ routes/board.js
@@ -112,7 +112,7 @@ router.delete("/delete/:bid", async (req, res) => {
 
 //메인 피드 최신순 게시글 6개
 router.get("/get/main", async (req, res) => {
-  let { page, count } = req.query;
+  let { userId, page, count } = req.query;
   if (!count) {
     count = 6;
   }
@@ -120,30 +120,38 @@ router.get("/get/main", async (req, res) => {
     page = 0;
   }
 
-  await asyncSQL(
-    `SELECT 
-          b.b_id as bid,
-          b.b_comment as content,
-          b.b_img as bimg,
-          b.b_date as date,
-          a.u_id as uid,
-          a.u_img as uimg,
-          a.u_email as email
-        FROM Board b JOIN User a
-        ON b.b_uid = a.u_id
-        ORDER BY b_date DESC
-        LIMIT ${page * count}, ${count}`
-  )
-    .then((rows) => {
-      res.status(200).json({
+  await asyncSQL(`
+  SELECT 
+    b.b_id as bid,
+    b.b_comment as content,
+    b.b_img as bimg,
+    b.b_date as date,
+    a.u_id as uid,
+    a.u_img as uimg,
+    a.u_email as email,
+    (SELECT COUNT(*) FROM Follow WHERE f_follower = '${parseInt(userId)}' 
+    AND f_following = a.u_id) AS following,
+    (SELECT COUNT(*) as count FROM \`b_Like\` WHERE bl_bid = bid AND bl_uid = '${parseInt(
+      userId
+    )}') AS lk
+  FROM Board b JOIN User a
+  ON b.b_uid = a.u_id
+  ORDER BY b_date DESC
+  LIMIT ${page * count}, ${count}
+`)
+    .then((rows, err) => {
+      console.log(err);
+      return res.status(200).json({
         status: "success",
         content: rows,
         nextPage: Number(page) + 1, // 다음 페이지 번호
         nextCount: Number(count), // 다음 페이지에 보여줄 개수
+        following: rows[0].following, // 해당 작성자를 팔로우하고 있는지 여부
+        lk: rows[0].lk,
       });
     })
     .catch((err) => {
-      res.status(500).json({
+      return res.status(500).json({
         status: "fail",
         message: "서버에서 에러가 발생 하였습니다.",
       });
--- test1.js
+++ test1.js
@@ -1,84 +1,27 @@
-router.get("/get/board/:bid/:uid", (req, res) => {
-  const { bid, uid } = req.params;
-
-  asyncSQL(
-    `
-    SELECT
-      b.b_id as bid,
-      b.b_comment as content,
-      b.b_img as bimg,
-      b.b_date as date,
-      u.u_img as uimg,
-      IFNULL(l.bl_bid, 0) as isLiked 
-    FROM Board b JOIN User u
-    ON b.b_uid = u.u_id
-    LEFT JOIN b_Like l
-    ON b.b_id = l.bl_bid AND l.bl_uid = "${uid}"
-    WHERE b.b_id = "${bid}"
-  `,
-    (err, rows) => {
-      if (err) {
-        res.status(500).json({
-          status: "fail",
-          message: "서버에서 에러가 발생 하였습니다.",
-        });
-        if (process.env.NODE_ENV === "development") {
-          console.error(err);
-        }
-      } else if (rows.length > 0) {
-        res.status(200).json({
-          status: "success",
-          bid: rows[0].bid,
-          content: rows[0].content,
-          date: rows[0].date,
-          isLiked: rows[0].isLiked ? true : false,
-        });
-      } else {
-        res.status(200).json({
-          status: "fail",
-          message: "데이터가 없습니다.",
-        });
-      }
-    }
-  );
-});
-
-router.post("/like", (req, res) => {
-  const { uid, bid } = req.body;
+// 해당 게시글의 좋아요 여부 확인
+router.get("/like/check", (req, res) => {
+  const { uid, bid } = req.query;
   if (!uid || !bid) {
     return res.status(400).end();
   }
 
   asyncSQL(`
-    INSERT INTO \`b_Like\` (bl_bid, bl_uid) VALUES ("${bid}", "${uid}")
+    SELECT COUNT(*) as count FROM \`b_Like\` WHERE bl_bid = ${bid} AND bl_uid = ${uid}
   `)
-    .then(() => {
-      asyncSQL(`
-        UPDATE Board SET b_like = "${uid}" WHERE b_id = "${bid}"
-      `)
-        .then(() => {
-          res.status(201).json({
-            status: "success",
-            message: "좋아요가 등록되었습니다.",
-          });
-        })
-        .catch((err) => {
-          if (process.env.NODE_ENV === "development") {
-            console.error(err);
-          }
-          res.status(500).json({
-            status: "fail",
-            message: "서버에서 에러가 발생 했습니다.",
-          });
-        });
+    .then(([row]) => {
+      const isLiked = row.count > 0;
+      res.status(200).json({
+        status: "success",
+        isLiked: isLiked,
+      });
     })
     .catch((err) => {
-      if (process.env.NODE_ENV === "development") {
-        console.error(err);
-      }
       res.status(500).json({
         status: "fail",
         message: "서버에서 에러가 발생 했습니다.",
       });
+      if (process.env.NODE_ENV === "development") {
+        console.error(err);
+      }
     });
 });
