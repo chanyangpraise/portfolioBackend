1e9647907f1bdffeee2d04db87162c178cd9d906
jg06 <jg06241@gmail.com>
2023-06-13 17:16:31 +0900

    대댓글 기능 수정

--- routes/re_comment.js
+++ routes/re_comment.js
@@ -23,12 +23,13 @@ router.get("/get/:cid", (req, res) => {
     SELECT
       r.re_id as reid, 
       u.u_id as uid, 
+      u.u_img as uimg,
       u.u_email as email, 
       r.re_comment as content, 
       r.re_date as date
     FROM Re_comment r JOIN user u
     ON r.re_uid = u.u_id
-    WHERE r.re_cid = "${bid}"
+    WHERE r.re_cid = "${cid}"
     ORDER BY r.re_date DESC
     LIMIT ${page * count}, ${count}
   `
@@ -90,7 +91,7 @@ router.post("/write", async (req, res) => {
 // 사용자를 확인 (본인 일치)
 // -> 본인 일치 확인하기 위해서 userId
 router.put("/fix/:re_cid", (req, res) => {
-  const { recid } = req.params;
+  const { re_cid } = req.params;
   const { userId, content } = req.body;
 
   // 본인 확인
@@ -98,17 +99,15 @@ router.put("/fix/:re_cid", (req, res) => {
     `SELECT
       re_uid
     FROM Re_comment
-    WHERE re_cid = ${recid}
+    WHERE re_cid = ${re_cid}
   `
   )
     .then((rows) => {
       if (rows.length > 0) {
-        if (rows[0].c_uid === Number(userId)) {
+        if (rows[0].re_uid === Number(userId)) {
           asyncSQL(
-            `
-            UPDATE Re_comment SET re_comment = "${content}"
-            WHERE re_cid = "${recid}"
-          `
+            `UPDATE Re_comment SET re_comment = ? WHERE re_cid = ?`,
+            [content, re_cid]
           )
             .then(() => {
               res.status(200).json({
@@ -147,49 +146,41 @@ router.put("/fix/:re_cid", (req, res) => {
 // 댓글 삭제
 // 어떤 댓글인지 알아야됨 -> cId
 // 쓴 사람이 지운건지? -> uId
-router.delete("/delete/:re_cid", (req, res) => {
-  const { recid } = req.params;
+router.delete("/delete/:re_cid", async (req, res) => {
+  const { re_cid } = req.params;
   const { uid } = req.query;
-  if (!recid || !uid) {
+
+  if (!re_cid || !uid) {
     res.status(400).end();
+    return;
   }
 
-  asyncSQL(`SELECT re_uid FROM Re_comment WHERE re_cid = ${recid}`)
-    .then((rows) => {
-      if (rows.length > 0) {
-        if (rows[0].re_uid === Number(uid)) {
-          asyncSQL(`DELETE FROM Re_comment WHERE re_cid = "${recid}"`)
-            .then(() => {
-              res.status(200).json({
-                status: "success",
-                message: "성공적으로 바뀌었습니다.",
-              });
-            })
-            .catch((err1) => {
-              res.status(500).json({
-                status: "fail",
-                message: "서버에서 에러가 발생 했습니다.",
-              });
-              if (process.env.NODE_ENV === "development") {
-                console.error(err1);
-              }
-            });
-        } else {
-          res.status(403).end();
-        }
+  try {
+    const rows = await asyncSQL(`SELECT re_uid FROM Re_comment WHERE re_id = ?`, 
+    [re_cid]);
+    if (rows && rows.length > 0) {
+      if (rows[0].re_uid === Number(uid)) {
+        await asyncSQL(`DELETE FROM Re_comment WHERE re_id = ?`, [re_cid]);
+        res.status(200).json({
+          status: "success",
+          message: "삭제되었습니다.",
+        });
       } else {
         res.status(403).end();
       }
-    })
-    .catch((err) => {
-      res.status(500).json({
-        status: "fail",
-        message: "서버에서 에러가 발생 했습니다.",
-      });
-      if (process.env.NODE_ENV === "development") {
-        console.error(err);
-      }
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: "서버에서 에러가 발생했습니다.",
     });
+    if (process.env.NODE_ENV === "development") {
+      console.error(err);
+    }
+  }
 });
 
 module.exports = router;
+
